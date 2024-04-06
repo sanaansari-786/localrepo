3a056900792824543b2c64fa5b60fd0539ebcb19
sana ansari <sana643058@gmail.com>
2024-04-06 12:48:08 +0530

    edit file

---
+++ OneDrive/Desktop/sana ansari/.vscode/my_first_project/src/loops.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class main{
+ /**
+     *
+     */
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+public static void main(String[]args){   
+      Scanner sc = SCANNER;
+int x=sc.nextInt();
+if(x%2==0){
+    System.out.println(x +"even number");
+}
+
+    
+        
+    }
+}
